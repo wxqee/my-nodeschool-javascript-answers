f304066d556e5c55cb4385c33cc26692988d3c24
Wang Xiaoqiang <xq21@vip.qq.com>
2014-12-30 17:34:28 +0800

    answer to currying

---
+++ functional-javascript-workshop/Currying/program.js
@@ -0,0 +1,20 @@
+function curryN(fn, n) {
+  if (typeof n != 'number') {
+    n = fn.length;
+  }
+
+  function getCurriedFn(prev) {
+    return function(arg) {
+      var args = prev.concat(arg);
+      if (args.length < n) {
+        return getCurriedFn(args);
+      } else {
+        return fn.apply(this, args);
+      }
+    };
+  }
+
+  return getCurriedFn([]);
+}
+
+module.exports = curryN;
